807018026576578dc27817e6aa70a258096e1d58
Andreas Sehr <andreas@softbrix.se>
2018-08-02 22:28:21 +0200

    Better error handling and returning file size

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 const util = require('util');
 
+var fstat = util.promisify(fs.fstat);
 var open = util.promisify(fs.open);
 var read = util.promisify(fs.read);
 var close = util.promisify(fs.close);
@@ -9,12 +10,18 @@ const DEBUG = false;
 const BUFFER_SIZE = 8 * 1024;
 
 function getBufferName(buffer) {
-	let end = buffer.indexOf(0x00, 'hex');
+    let skip = 4;   // Ignore the APP0 Header blocks (marker, size)
+	let end = buffer.indexOf(0x00, skip, 'hex');
 	if(end > 0) {
-		return buffer.slice(0, end).toString('utf8')
+		return buffer.slice(skip, end).toString('utf8')
 	}
 	return 'unknown';
 }
+
+async function readSize(fd) {
+    let fst = await fstat(fd);
+    return fst.size;
+} 
 
 async function loadBlocksFromFile (fd, buffer) {
     var offset = 0, 
@@ -27,38 +34,40 @@ async function loadBlocksFromFile (fd, buffer) {
         // Read the file, callback methods has been promisified with util and returns both the buffer and the bytes read
         return (await read(fd, buffer, 0, buffer.length, fileOffset)).bytesRead;
     }
+    let bufferSize = await readSize(fd);
+    if (!bufferSize || bufferSize < 8) {
+        throw new Error('Too few bytes: ' + bufferSize);
+    }
+    blocks['size'] = bufferSize;
     let bytesRead = await readFile(0);
     if (!bytesRead || bytesRead < 8) {
-        if (DEBUG) console.log("Too few bytes loaded: " + bytesRead);
-        return null;
+        throw new Error('Too few bytes loaded: ' + bytesRead);
     }
     // check for jpeg magic bytes header
     if (buffer[offset++] != 0xFF || buffer[offset++] != 0xD8) {
-        if (DEBUG) console.log("Buffer is not a valid JPEG");
-        return false; // it is not a valid jpeg
+        throw new Error("Buffer is not a valid JPEG");
     }
     
     // Loop through the file looking for the photoshop header bytes
     while (offset < bytesRead) {
         if (buffer[offset] != 0xFF) {
-            if (DEBUG) console.log("Not a valid marker at offset " + offset + ", found: " + buffer[offset]);
-            return false;
+            throw new Error("Not a valid marker at offset " + offset + ", found: " + buffer[offset]);
         }
 
         let blockMarker = buffer[++offset];
          // Start of image scan, end the loop
-        if (blockMarker === 0xDA) {
+        if (blockMarker === 0xD9 /* EOI */ || blockMarker === 0xDA /* SOS */) {
             if(DEBUG) console.log('Start of image', fileOffset + offset);
             break;
         }
         let name = blockMarker;
         // Read block size and load into memory
         let size = buffer.readUInt16BE(++offset);
-        let metaBuffer = new Buffer(size - 2);
-        await read(fd, metaBuffer, 0, metaBuffer.length, fileOffset + offset + 2);
+        let metaBuffer = Buffer.alloc(size + 2);
+        await read(fd, metaBuffer, 0, metaBuffer.length, fileOffset + offset - 2);
         
         // Load the block name if it has one
-        if(blockMarker === 0xE0 || blockMarker === 0xE1 || blockMarker === 0xE2) {
+        if(blockMarker >= 0xE0 && blockMarker <= 0xE9) {
             name = getBufferName(metaBuffer);
         }
         blocks[name] = metaBuffer;
@@ -76,10 +85,11 @@ module.exports = async function(filePath) {
     let fd;
 	try {
 		fd = await open(filePath, 'r');
-		const buffer = new Buffer(BUFFER_SIZE);
+		const buffer = Buffer.alloc(BUFFER_SIZE);
 		return await loadBlocksFromFile(fd, buffer);
 	} catch (ex) {
-		console.error(ex);
+        if(DEBUG) console.error(ex);
+        throw ex;
 	} finally {
 		if(fd) {
 			close(fd);
--- test/test.js
+++ test/test.js
@@ -2,12 +2,23 @@ const assert = require('assert');
 const metaReader = require('../');
 
 let fixture = __dirname + '/test.jpg';
+let non_fixture = __dirname + '/does_not_exist.jpg';
 
 describe('hemmesta-jpeg-meta', function() {
     it('should extract all meta information blocks from the jpeg', function() {
         return metaReader(fixture).then((result) => {
-            // console.log(result);
-            assert.equal(Object.keys(result).length, 8);
+            assert.equal(Object.keys(result).length, 9);
+            assert.equal(result['size'], 93696);
+        });
+    });
+
+    it('should report if file is invalid', function() {
+        metaReader(non_fixture).then((result) => {
+            assert.fail('Did not report ivalid file');
+        }).catch(ex => {
+            assert.ok(ex);
+            assert(ex.message);
+            assert(ex.message.startsWith('ENOENT: no such file or directory, open'));
         });
     });
 });
